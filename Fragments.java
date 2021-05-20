61ca6ed059cec8030590ee5bd9c1e42eaffc35dc
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-05-20 14:33:32 +0200

    Clean up and JDoc additions

--- Fragments.java
+++ Fragments.java
@@ -1,12 +1,27 @@
 import java.io.IOException;
 import java.util.Arrays;
 
+/**
+ * This class checks the execution times of pre-defined code fragments.
+ * The fragments loop a set amount of numbers and count each pass-through.
+ * This is used to demonstrate the Big-Oh analysis in numbers.
+ * 
+ * @author Lab Partner D.
+ * @author n-cßde-r
+ * @version 19.15.2021
+ *
+ */
+
 public class Fragments {
 
 	public static void main(String[] args) throws IOException {
 		checkFragments();
 	}
 
+	/**
+	 * loops through all fragment methods and runs them for a specific set of times,
+	 * returned values are sums of number of loop-throughs.
+	 */
 	private static void checkFragments() {
 		int[] numbers = { 10, 100, 1000, 10000, 100000 }; // Create a list of numbers to sum up
 		long[] frag1 = new long[5]; 
@@ -18,57 +33,41 @@ public class Fragments {
 		long[] frag7 = new long[5];
 		long[] frag8 = new long[5]; // respective runs get stored.
 		
-		//Run fragment one and print the results
+		//Run fragment one to eight, except 5 and 7
 		for (int i = 0; i < numbers.length; i++) {
 			frag1[i] = fragmentOne(numbers[i]);
-		}
-		System.out.println("The sums of fragment nr 1 are: " + Arrays.toString(frag1));
-		
-		//Run fragment two and print the results
-		for (int i = 0; i < numbers.length; i++) {
 			frag2[i] = fragmentTwo(numbers[i]);
-		}
-		System.out.println("The sums of fragment nr 2 are: " + Arrays.toString(frag2));
-		
-		//Run fragment three and print the results
-		for (int i = 0; i < numbers.length; i++) {
 			frag3[i] = fragmentThree(numbers[i]);
-		}
-		System.out.println("The sums of fragment nr 3 are: " + Arrays.toString(frag3));
-
-		//Run fragment four and print the results
-		for (int i = 0; i < numbers.length; i++) {
 			frag4[i] = fragmentFour(numbers[i]);
+			frag6[i] = fragmentSix(numbers[i]);
+			frag8[i] = fragmentEight(numbers[i]);
 		}
-		System.out.println("The sums of fragment nr 4 are: " + Arrays.toString(frag4));
 
-		//Run fragment five only a few of the designated times, as it runs almost
+		//Run fragment five and seven only a few of the designated times, as it runs almost
 		// "endlessly", so to save time and estimate our guesses, runs are limited, and print the results
 		for (int i = 0; i < 3; i++) {
 			frag5[i] = fragmentFive(numbers[i]);
-		}
-		System.out.println("The sums of fragment nr 5 are: " + Arrays.toString(frag5));
-
-		//Run fragment six and print the results
-		for (int i = 0; i < numbers.length; i++) {
-			frag6[i] = fragmentSix(numbers[i]);
-		}
-		System.out.println("The sums of fragment nr 6 are: " + Arrays.toString(frag6));
-
-		//Run fragment seven as limited as nr 5 and print the results
-		for (int i = 0; i < 3; i++) {
 			frag7[i] = fragmentSeven(numbers[i]);
 		}
+		
+		//Print all the sums in order. Takes time, be patient!
+		System.out.println("The sums of fragment nr 1 are: " + Arrays.toString(frag1));
+		System.out.println("The sums of fragment nr 2 are: " + Arrays.toString(frag2));
+		System.out.println("The sums of fragment nr 3 are: " + Arrays.toString(frag3));
+		System.out.println("The sums of fragment nr 4 are: " + Arrays.toString(frag4));
+		System.out.println("The sums of fragment nr 5 are: " + Arrays.toString(frag5));
+		System.out.println("The sums of fragment nr 6 are: " + Arrays.toString(frag6));
 		System.out.println("The sums of fragment nr 7 are: " + Arrays.toString(frag7));
-
-		//Run fragment eight and print the results
-		for (int i = 0; i < numbers.length; i++) {
-			frag8[i] = fragmentEight(numbers[i]);
-		}
 		System.out.println("The sums of fragment nr 8 are: " + Arrays.toString(frag8));
+		
 	}
 
 	//Methods of code fragments to be checked for Big-Ohs
+	/**
+	 * Fragment to check for loop efficiency and execution times.
+	 * @param n Times of loops
+	 * @return  sum of all loop times as a long
+	 */
 	private static long fragmentOne(int n) { // O(n)
 		long sum = 0;
 		for (int i = 0; i < n; i++) {
@@ -77,6 +76,11 @@ public class Fragments {
 		return sum;
 	}
 
+	/**
+	 * Fragment to check for loop efficiency and execution times.
+	 * @param n Times of loops
+	 * @return  sum of all loop times as a long
+	 */
 	private static long fragmentTwo(int n) { // O(n*n)
 		long sum = 0;
 		for (int i = 0; i < n; i++) {
@@ -87,6 +91,11 @@ public class Fragments {
 		return sum;
 	}
 
+	/**
+	 * Fragment to check for loop efficiency and execution times.
+	 * @param n Times of loops
+	 * @return  sum of all loop times as a long
+	 */
 	private static long fragmentThree(int n) { // (n*n)/2 + n/2 - j startet immer bei it, dadurch geht er nur n/2 mal
 		long sum = 0; 							// durch (Im Prinzip wird immer jede Zahl < i Ã¼bersprungen in der 2.
 		for (int i = 0; i < n; i++) { 			// Schleife) // und durch die erste Schleife n mal, also n*n/2
@@ -97,6 +106,11 @@ public class Fragments {
 		return sum;
 	}
 
+	/**
+	 * Fragment to check for loop efficiency and execution times.
+	 * @param n Times of loops
+	 * @return  sum of all loop times as a long
+	 */
 	private static long fragmentFour(int n) { // (n*n) + n, da der Code wie bei fragmentTwo quadratisch ist, aber dabei
 		long sum = 0;						  // noch die Iteration zÃ¤hlt, bei der Sie n mal durch die erste Schleife geht
 		for (int i = 0; i < n; i++) {
@@ -108,6 +122,11 @@ public class Fragments {
 		return sum;
 	}
 
+	/**
+	 * Fragment to check for loop efficiency and execution times.
+	 * @param n Times of loops
+	 * @return  sum of all loop times as a long
+	 */
 	private static long fragmentFive(int n) { // (n*n*n) da der Code durch die erste Schleife n mal geht,
 		long sum = 0;						  // durch die zweite aben n*n Mal
 		for (int i = 0; i < n; i++) {
@@ -118,6 +137,11 @@ public class Fragments {
 		return sum;
 	}
 
+	/**
+	 * Fragment to check for loop efficiency and execution times.
+	 * @param n Times of loops
+	 * @return  sum of all loop times as a long
+	 */
 	private static long fragmentSix(int n) { // idk iwas in richtung n*n/2
 		long sum = 0;
 		for (int i = 0; i < n; i++) {
@@ -128,6 +152,11 @@ public class Fragments {
 		return sum;
 	}
 
+	/**
+	 * Fragment to check for loop efficiency and execution times.
+	 * @param n Times of loops
+	 * @return  sum of all loop times as a long
+	 */
 	private static long fragmentSeven(int n) { // idk iwas richtung n*n*n
 		long sum = 0;
 		for (int i = 1; i < n; i++) {
@@ -142,6 +171,11 @@ public class Fragments {
 		return sum;
 	}
 
+	/**
+	 * Fragment to check for loop efficiency and execution times.
+	 * @param n Times of loops
+	 * @return  sum of all loop times as a long
+	 */
 	private static long fragmentEight(int n) { // n/2^x = 1 -> n = 2^x -> x = log 2 zu n (dabei wird immer abgerundet)
 		long sum = 0;
 		int i = n;
