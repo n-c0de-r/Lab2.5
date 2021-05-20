97922c5e78dbb01aad50fab66101d8216874713e
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-05-20 15:08:57 +0200

    corrected the loop, added a primitive check

--- primeNumber.java
+++ primeNumber.java
@@ -1,9 +1,25 @@
+
 public class primeNumber {
-    public static boolean isPrime(int p){                  // worst case if n is a prime number - running time would be n long
-        for(int n = 0; n <= p-1; n++){
-            if(p%n == 0){
-                return false;
-            }
-        } return true;
-    }
+
+	public static void main(String[] args) {
+		checkPrime();
+	}
+
+	private static void checkPrime() {
+
+		for (int i = 1; i < 1000000; i++) {
+			if (isPrime(i)) {
+				System.out.println(i + " is a prime.");
+			}
+		}
+	}
+
+	public static boolean isPrime(int p) { // worst case if n is a prime number - running time would be n long
+		for (int n = 2; n <= p/2; n++) {
+			if (p % n == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
